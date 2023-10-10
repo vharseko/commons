007ca31f4dc2d1c26f7767d7d88a763259499669
Valera V Harseko <vharseko@3a-systems.ru>
2023-10-10 19:14:54 +0300

    try allow windows

--- cassandra-embedded/src/main/java/org/openidentityplatform/commons/cassandra/EmbeddedServer.java
+++ cassandra-embedded/src/main/java/org/openidentityplatform/commons/cassandra/EmbeddedServer.java
@@ -25,10 +25,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.Arrays;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.apache.cassandra.config.DatabaseDescriptor;
